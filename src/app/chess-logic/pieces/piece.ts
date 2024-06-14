0d2dd422e1d4b6891a045ddf707004479c6fc939
alexandernoack <alexander.noack@amotiq.de>
2024-06-14 15:47:09 +0200

    - created abstract piece class and a model for it

---
+++ src/app/chess-logic/pieces/piece.ts
@@ -0,0 +1,22 @@
+import { Color, Coords, FENChar } from '../models';
+
+export abstract class Piece {
+    protected abstract _FENChar: FENChar;
+    protected abstract _direction: Coords[];
+
+    constructor(private _color: Color){
+
+    }
+
+    public get FENChar(): FENChar {
+        return this._FENChar;
+    }
+
+    public get direction(): Coords[] {
+        return this._direction;
+    }
+
+    public get color(): Color {
+        return this._color;
+    }
+}
