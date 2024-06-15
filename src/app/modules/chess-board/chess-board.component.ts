165b9e5b1e42598cb26d4eb9241a03887378f03f
alexandernoack <alexander.noack@amotiq.de>
2024-06-15 18:51:19 +0200

    added chess board component

---
+++ src/app/modules/chess-board/chess-board.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ChessBoard } from 'src/app/chess-logic/chess-board';
+import { Color, FENChar } from 'src/app/chess-logic/models';
+
+@Component({
+  selector: 'app-chess-board',
+  templateUrl: './chess-board.component.html',
+  styleUrls: ['./chess-board.component.css']
+})
+export class ChessBoardComponent implements OnInit {
+  private chessBoard = new ChessBoard();
+  public chessBoardView: (FENChar | null)[][] = this.chessBoard.chessBoardView;
+  public get playerColor(): Color{return this.chessBoard.playerColor};
+
+  public isSquareDark(x: number, y: number): boolean {
+    return ChessBoard.isSquareDark(x, y);
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
